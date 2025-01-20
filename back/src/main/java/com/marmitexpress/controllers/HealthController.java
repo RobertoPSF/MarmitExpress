89b64c48aa04b6087645343e1be3437d977fdd89
JVSMOTA <jvmota.sb@gmail.com>
2025-01-20 16:59:50 -0300

    Adding CORS to enable health requests to the frontend

--- back/src/main/java/com/marmitexpress/controllers/HealthController.java
+++ back/src/main/java/com/marmitexpress/controllers/HealthController.java
@@ -3,9 +3,11 @@ package com.marmitexpress.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/health")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class HealthController {
 
     @GetMapping
