d01df4addd4b009bac66daf05494444d8897ff7b
JVSMOTA <jvmota.sb@gmail.com>
2025-01-20 17:00:23 -0300

    Creating Status Page

--- front/src/components/JustifyContainer/styles.tsx
+++ front/src/components/JustifyContainer/styles.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
   align-items: center;
   justify-content: space-between;
 `;
