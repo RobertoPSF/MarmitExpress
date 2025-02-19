a74fdbc7af743edcb94caf97e29f1e9d86c069f7
JVSMOTA <jvmota.sb@gmail.com>
2025-02-18 22:19:40 -0300

    Updating directories to improve organization

---
+++ front/src/components/Forms/ClienteLogin/index.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Button from "../../Button";
+import Input from "../../Input";
+
+interface ClienteLoginProps {
+    onClose: () => void;
+}
+
+const ClienteLogin: React.FC<ClienteLoginProps> = ({ onClose }) => {
+    const [formDataLogin, setFormDataLogin] = useState({
+        usuario: '',
+        senha: ''
+      });
+
+    const handleChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormDataLogin({ ...formDataLogin, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmitLogin = async () => {
+        try {
+          const response = await fetch('http://localhost:8080/clientes/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              usuario: formDataLogin.usuario,
+              senha: formDataLogin.senha
+            })
+          });
+    
+          if (response.ok) {
+            alert('Login realizado com sucesso!');
+            onClose();
+          } else {
+            alert('Erro ao fazer Login. Verifique os dados e tente novamente.');
+          }
+        } catch (error) {
+          console.error('Erro na requisição:', error);
+          alert('Erro ao conectar com o servidor.');
+        }
+      };
+  
+  return (
+    <>
+        <Input
+            placeHolderContainer="Usuário"
+            name="usuario"
+            type="text"
+            placeholder="Nome de usuário"
+            value={formDataLogin.usuario}
+            onChange={handleChangeLogin}
+        />
+
+        <Input
+            placeHolderContainer="Senha"
+            name="senha"
+            type="password"
+            placeholder="******"
+            value={formDataLogin.senha}
+            onChange={handleChangeLogin}
+        />
+
+        <a>Esqueceu a senha?</a>
+
+        <Button type={'orange'} onClick={handleSubmitLogin}>
+            Continuar
+        </Button>
+    </>
+  );
+};
+
+export default ClienteLogin;
+
+
