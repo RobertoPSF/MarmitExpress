36a0193c18723d9a610053f33d83178c4bbbe9ad
JVSMOTA <jvmota.sb@gmail.com>
2025-02-20 08:52:16 -0300

    Adding stylesheet to Meus Pedidos Page

--- front/src/pages/MeusPedidos/index.tsx
+++ front/src/pages/MeusPedidos/index.tsx
@@ -1,9 +1,37 @@
-import { Container } from './styles';
+import { Container, ModalContainer } from './styles';
+import Button from '../../components/Button';
+import ClienteLoginPopup from '../../components/PopUps/ClienteLoginPopUp';
+import { useState } from 'react';
 
 export default function MeusPedidos() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  const toggleLoginPopup = () => {
+    setIsLoginOpen(!isLoginOpen);
+  };
+
   return (
     <>
-      <Container></Container>
+      <Container>
+        <ModalContainer>
+          <h3>Entre na sua conta para acessar</h3>
+          <p>
+            É necessário que você entre na sua conta para ter acesso aos seus
+            pedidos!
+          </p>
+          <Button
+            type="orange"
+            children={'Entrar'}
+            onClick={toggleLoginPopup}
+          />
+          {isLoginOpen && (
+            <ClienteLoginPopup
+              isOpen={isLoginOpen}
+              onClose={toggleLoginPopup}
+            />
+          )}
+        </ModalContainer>
+      </Container>
     </>
   );
 }
