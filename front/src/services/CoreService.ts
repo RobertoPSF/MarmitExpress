2e7134330ab6c8d4a453ff00e2726c4dee00ab2a
JVSMOTA <jvmota.sb@gmail.com>
2025-02-24 19:15:08 -0300

    Create services system

---
+++ front/src/services/CoreService.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance } from 'axios';
+import config from '../config.json'; // Ajuste o caminho se necessário
+import Cookies from 'js-cookie'; // Para acessar o token JWT dos cookies
+
+const API_URL = config.API_LOCAL;
+
+class CoreService {
+  protected api: AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: API_URL,
+      timeout: 1000 * 10,
+    });
+
+    this.api.interceptors.request.use((config) => {
+      const token = Cookies.get('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+  }
+
+  getApi(): AxiosInstance {
+    return this.api;
+  }
+}
+
+export default CoreService;
