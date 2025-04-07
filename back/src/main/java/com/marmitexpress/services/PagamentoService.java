b9686678efc18bed1182757fe8a9a30ee059ebba
RobertoPSF <88672689+RobertoPSF@users.noreply.github.com>
2025-04-07 12:07:49 -0300

    Removing unused import

--- back/src/main/java/com/marmitexpress/services/PagamentoService.java
+++ back/src/main/java/com/marmitexpress/services/PagamentoService.java
@@ -9,7 +9,6 @@ import com.marmitexpress.repositorys.PedidoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
 
 @Service
 public class PagamentoService {
