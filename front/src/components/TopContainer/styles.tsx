f18d6116ca297b142851a648de0d458d5c33eed2
JVSMOTA <jvmota.sb@gmail.com>
2025-06-13 18:59:48 -0300

    style: ajustes visuais e botões para voltar

--- front/src/components/TopContainer/styles.tsx
+++ front/src/components/TopContainer/styles.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import frase from '../../assets/frase-home.svg?react';
 import logo from '../../assets/logo-marmitexpress.svg?react';
+import { Icon } from '@iconify/react';
 
-export const Container = styled.div<{ $isHome: boolean }>`
+export const Container = styled.div<{ $isHome: boolean,  $isCardapio: boolean }>`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -15,6 +16,35 @@ export const Container = styled.div<{ $isHome: boolean }>`
   h1 {
     margin-left: 12vw;
     margin-top: 7.5vh;
+  }
+
+  button {
+    z-index: 900;
+    display: flex;
+    align-items: center;
+    width: fit-content;
+    margin-left: ${({ $isCardapio }) =>
+      $isCardapio ? '6vw' : '12vw'};
+    margin-top: ${({ $isCardapio }) =>
+      $isCardapio ? '4vh' : '7.5vh'};
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: white;
+    gap: 10px;
+
+    h1 {
+      margin: 0;
+    }
+
+    &:hover {
+      text-decoration: underline;
+      opacity: 0.6;
+      transform: scale(1.05);
+    }
+  
+    transition: all 0.2s ease-in-out;
+
   }
 `;
 
@@ -36,4 +66,12 @@ export const Logo = styled(logo)`
   z-index: 1;
   top: 50vh;
   bottom: 50vh;
+`;
+
+export const StyledIcon = styled(Icon)`
+  color: #F6821F;
+  background-color: white;
+  border-radius: 50%;
+  height: min-content;
+  width: 30px;
 `;
